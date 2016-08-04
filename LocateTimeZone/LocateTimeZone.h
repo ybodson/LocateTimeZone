fb6d485c0beebf62c72cb2b23538894810846d18
Yann Bodson <y.bodson@me.com>
2016-08-05 09:47:37 +1000

    Initial Commit

---
+++ LocateTimeZone/LocateTimeZone.h
@@ -0,0 +1,19 @@
+//
+//  LocateTimeZone.h
+//  LocateTimeZone
+//
+//  Created by Yann Bodson on 5/08/2016.
+//  Copyright © 2016 frogmojo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LocateTimeZone.
+FOUNDATION_EXPORT double LocateTimeZoneVersionNumber;
+
+//! Project version string for LocateTimeZone.
+FOUNDATION_EXPORT const unsigned char LocateTimeZoneVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocateTimeZone/PublicHeader.h>
+
+
